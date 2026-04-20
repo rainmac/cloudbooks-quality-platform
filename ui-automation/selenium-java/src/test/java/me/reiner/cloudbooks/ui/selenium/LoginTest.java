767d8a480429db760b2b6a85f82dbdffa66fbfad
Reiner Tolentino <reiner.tolentino@gmail.com>
2026-04-20 10:24:08 +0800

    initial test

---
+++ ui-automation/selenium-java/src/test/java/me/reiner/cloudbooks/ui/selenium/LoginTest.java
@@ -0,0 +1,32 @@
+package me.reiner.cloudbooks.ui.selenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void testLogin() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com");
+		
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+		
+		WebElement lnkShoppingCart = driver.findElement(By.className("shopping_cart_link"));
+		Assert.assertTrue(lnkShoppingCart.isDisplayed());
+		
+		driver.quit();
+	}
+
+}
